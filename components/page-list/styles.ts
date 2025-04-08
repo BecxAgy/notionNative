c86424cd50e9b2076a16273974a36f87a3dbc66f
Rebeca <beca.cout@gmail.com>
2025-04-08 17:16:47 -0300

    refactor styles in page list

--- components/page-list/styles.ts
+++ components/page-list/styles.ts
@@ -1,3 +1,30 @@
 import { StyleSheet } from "react-native";
+import { colors } from '@/styles/colors';
 
-export const s = StyleSheet.create({        })
+export const s = StyleSheet.create({
+  container: {
+    padding: 20,
+    gap: 12,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 16,
+  },
+  title: {
+    fontSize: 16,
+    color: colors.gray[300],
+    flex: 1,
+  },
+  listContainer: {
+    backgroundColor: colors.gray[700],
+    borderRadius: 10,
+  },
+  separator: {
+    height: 1,
+    backgroundColor: colors.gray[600],
+  },
+  icon: {
+    color: colors.gray[300],
+  },
+});
